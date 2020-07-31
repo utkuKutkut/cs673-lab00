45f1cc3e9c8666a21cc87685106ebf1ce3e218d6
utkuKutkut <utkucankutkut@hotmail.com>
2020-07-31 18:54:04 +0300

    Task 1 partially completed. Could not figure out a solution for integer overflow problem ... (last test case fails.)

--- src/main/java/edu/usfca/cs673/Task1.java
+++ src/main/java/edu/usfca/cs673/Task1.java
@@ -11,39 +11,39 @@ public class Task1 {
   // TODO: Feel free to declare a new variable, if needed.
 
   public static int getNumberOfWaysRecursive(int n) {
-    //Equation: 2x+y=n
-    int sum=0;
-    long x=n/2;
-    long y=n%2;
-    long loopNumber=(n/2)+1;
-    for(int i=0;i<loopNumber;i++){
-      sum+=comb(x+y,min(x,y));
-      x-=1;
-      y+=2;
+    if (n == 1) {
+      return 1;
+    } else if (n <= 0) {
+      return 0;
     }
-    return (sum)%MOD;
-  }
-  public static double comb(long x,long y){
-    long top=1;
-    long bottom=1;
-    long tempX=x;
-    long tempY=y;
-    for(int i=0;i<y;i++){
-      top=(top*tempX);
-      bottom=(bottom*tempY);
-      tempX-=1;
-      tempY-=1;
-    }
-    return top/bottom;
+    return getNumberOfWaysIterative(n - 1) + getNumberOfWaysIterative(n - 2);
   }
 
   public static int getNumberOfWaysIterative(int n) {
-    if(n==0){
-      return 1;
+    //Equation: 2x+y=n
+    int sum = 0;
+    long x = n / 2;
+    long y = n % 2;
+    long loopNumber = (n / 2) + 1;
+    for (int i = 0; i < loopNumber; i++) {
+      sum += comb(x + y, min(x, y));
+      x -= 1;
+      y += 2;
     }
-    else if(n<0){
-      return 0;
+    return (sum) % MOD;
+  }
+
+  public static double comb(long x, long y) {
+    long top = 1;
+    long bottom = 1;
+    long tempX = x;
+    long tempY = y;
+    for (int i = 0; i < y; i++) {
+      top = (top * tempX);
+      bottom = (bottom * tempY);
+      tempX -= 1;
+      tempY -= 1;
     }
-    return getNumberOfWaysIterative(n-1)+getNumberOfWaysIterative(n-2);
+    return top / bottom;
   }
 }
